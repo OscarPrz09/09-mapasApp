82a47aea28cda4696930c843f2bf2812f713dd04
OscarPrz09 <oscarprz2609@gmail.com>
2022-09-30 13:57:06 -0500

    Commit final de App

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, NgModule } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 import * as mapboxgl from 'mapbox-gl';
 import { environment } from 'src/environments/environment';
--- src/app/mapas/pages/zoom-range/zoom-range.component.ts
+++ src/app/mapas/pages/zoom-range/zoom-range.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnDestroy, ViewChild } from '@angular/core';
 import * as mapboxgl from 'mapbox-gl';
 
 @Component({
@@ -18,36 +18,63 @@ import * as mapboxgl from 'mapbox-gl';
       padding: 10px;
       position: fixed;
       z-index: 999;
+      width: 400px;
     }
     `
   ]
 })
-export class ZoomRangeComponent implements AfterViewInit {
+export class ZoomRangeComponent implements AfterViewInit, OnDestroy {
 
   @ViewChild('mapa') divMapa!: ElementRef;
   mapa!: mapboxgl.Map;
-  zoomLevel: number = 10;
+  zoomLevel: number = 15;
+  center: [number, number ] = [-75.62208355360754, 2.193330696656462];
 
   constructor() { }
+
+  ngOnDestroy(): void {
+    this.mapa.off('zoom', () => {});
+    this.mapa.off('zoomend', () => {});
+    this.mapa.off('move', () => {});
+  }
 
   ngAfterViewInit(): void {
     
     this.mapa = new mapboxgl.Map({
       container: this.divMapa.nativeElement, // container ID
       style: 'mapbox://1styles/mapbox/streets-v11',
-      center: [-75.62208355360754, 2.193330696656462], // style URL
+      center: this.center, // style URL
       zoom: this.zoomLevel, // starting zoom
       });
+
+      this.mapa.on('zoom', () => {
+        this.zoomLevel = this.mapa.getZoom();
+      })
+
+      this.mapa.on('zoomend', () => {
+        if ( this.mapa.getZoom() > 18) {
+          this.mapa.zoomTo( 18 );
+        }
+      })
+
+      //Movimiento del mapa
+      this.mapa.on('move', (event) => {
+        const target = event.target;
+        const { lng, lat } = target.getCenter();
+        this.center = [lng, lat];
+      })
   }
 
   zoomOut(){
     this.mapa.zoomOut();
-    this.zoomLevel = this.mapa.getZoom();
   }
 
   zoomIn(){
     this.mapa.zoomIn();
-    this.zoomLevel = this.mapa.getZoom();
+  }
+
+  zoomCambio(valor: string) {
+    this.mapa.zoomTo( Number(valor));
   }
 
 }
